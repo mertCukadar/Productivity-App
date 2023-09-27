c263c94527924745469a7976a5c69dd9d24e69cc
mertCukadar <cukadar.mertkaan@gmail.com>
2023-09-27 15:53:04 +0300

    main file login deneme

---
+++ ProductivityApp/src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Login from "./component/LoginRegister/Login.tsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>
+);
