bbd0fbe10de8f5f4e6caa3a7d878b3467897c4b5
mertCukadar <cukadar.mertkaan@gmail.com>
2023-10-01 14:53:08 +0300

    Timer component refactored

--- my-app/component/Timer.js
+++ my-app/component/Timer.js
@@ -84,18 +84,7 @@ export function Timer() {
           buttonText={running ? "Stop" : "Start"}
         />
 
-        <View style={styles.todoContainer}>
-          <View style={styles.leftSide}>
-            <CheckBox
-              checked={toggleCheckBox}
-              onPress={() => setToggleCheckBox(!toggleCheckBox)}
-              style={{ marginRight: 10 }}
-            />
-            <Text style={styles.itemText}>To do list item</Text>
-          </View>
 
-          <View style={styles.status}></View>
-        </View>
       </View>
     </LinearGradient>
   );
@@ -128,32 +117,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: '#19A7CE',
     fontWeight: 'bold',
-  },
-  todoContainer: {
-    backgroundColor: '#F6F1F1',
-    padding: 10,
-    borderRadius: 10,
-    width: 300,
-    marginTop: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  itemText: {
-    fontSize: 18,
-    color: 'black',
-  },
-  status: {
-    width: 20,
-    flexDirection: 'row',
-    height: 20,
-    backgroundColor: '#FF6969',
-    borderRadius: 10,
-  },
-  leftSide: {
-    flexWrap: 'wrap',
-    marginRight: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
   },
 });
