5768f8ea98361141eb101a6da01853ec420d6e8d
mertCukadar <cukadar.mertkaan@gmail.com>
2023-10-01 14:45:39 +0300

    app js timer component added

--- my-app/App.js
+++ my-app/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View , SafeAreaView } from 'react-native';
 import { Login } from './component/Login';
+import { Timer } from './component/Timer';
 
 export default function App() {
   return (
     <View style = {styles.container}>
-      <Login />
+      <Timer />
     </View>
 
 
