0ed74c0556679cfb3fec269f79d7bf7f7e0d78e8
mertCukadar <cukadar.mertkaan@gmail.com>
2023-10-03 23:01:58 +0300

    contribition copmonent added

--- my-app/App.js
+++ my-app/App.js
@@ -1,18 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { Login } from './component/pages/Login';
 import { Timer } from './component/Timer';
+import { ContributionChart } from './component/ContributionChart';
 import { Todo } from './component/pages/Todo';
 
 export default function App() {
   return (
     <View style = {styles.container}>
-        <Todo />
+        <ContributionChart />
     </View>
-
-
-    
-
   );
 
 
