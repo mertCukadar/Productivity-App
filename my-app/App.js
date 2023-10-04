2155176035607f5288d74f3fde3d41c5550dfcaa
mertCukadar <cukadar.mertkaan@gmail.com>
2023-10-05 00:40:46 +0300

    app js updated

--- my-app/App.js
+++ my-app/App.js
@@ -4,11 +4,12 @@ import { Login } from './component/pages/Login';
 import { Timer } from './component/Timer';
 import { ContributionChart } from './component/ContributionChart';
 import { Todo } from './component/pages/Todo';
+import { MainPage } from './component/pages/MainPage';
 
 export default function App() {
   return (
     <View style = {styles.container}>
-        <ContributionChart />
+        <MainPage />
     </View>
   );
 
