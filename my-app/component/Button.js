893382cb8cfaa2a0f67639489ec9b8b1083a3313
mertCukadar <cukadar.mertkaan@gmail.com>
2023-10-08 21:36:57 +0300

    new probs added

--- my-app/component/Button.js
+++ my-app/component/Button.js
@@ -2,11 +2,11 @@ import React from "react";
 import { StyleSheet, Text, Pressable } from 'react-native';
 
 export function CustomButton(props) {
-  const { onPress, buttonText, width, disabled } = props;
+  const { onPress, buttonText, width, disabled , borderRadius } = props;
 
   return (
     <Pressable
-      style={[styles.button, { width: width }]}
+      style={[styles.button, { width: width, borderRadius: borderRadius }]}
       onPress={onPress}
       disabled={disabled}
     >
@@ -18,9 +18,9 @@ export function CustomButton(props) {
 const styles = StyleSheet.create({
   button: {
     height: 40,
-    backgroundColor: "#19A7CE",
+    backgroundColor: "#7289DA",
     borderRadius: 5,
-    borderColor: "#19A7CE",
+    borderColor: "#7289DA",
     alignItems: "center",
     borderWidth: 1,
     textAlign: "center",
@@ -29,9 +29,9 @@ const styles = StyleSheet.create({
   },
   text: {
     color: "#F6F1F1",
-    fontSize: 15,
+    fontSize: 16,
     fontWeight: "bold",
     textAlign: "center",
-    marginTop: 5,
+    marginTop: 7,
   },
 });
