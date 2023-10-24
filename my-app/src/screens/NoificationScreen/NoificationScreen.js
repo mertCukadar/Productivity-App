d2c3680ef6fe7fd884acd816fa21f4ef985d0e13
mertCukadar <cukadar.mertkaan@gmail.com>
2023-10-24 22:17:35 +0300

    Notification page added

---
+++ my-app/src/screens/NoificationScreen/NoificationScreen.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, Text, Pressable, SafeAreaView , View, TextInput } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+
+export function NotificationScreen(props) {
+    return (
+        
+           <View style={styles.container}>
+                <Text style = {{color:"white"}}>Notification Screen</Text>
+            </View>
+        
+
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: 'center',
+        backgroundColor: '#24292E',
+    },
+});
+
