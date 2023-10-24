431491dbefd6a9b6e81a181c7383eeccfa206e2e
mertCukadar <cukadar.mertkaan@gmail.com>
2023-10-24 22:16:10 +0300

    App js las update

--- my-app/App.js
+++ my-app/App.js
@@ -1,66 +1,18 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {MainPage} from './src/screens/MainPage';
 import { CustomTabNavigator } from './src/component/CustomTabNavigator/customTabNavigator';
-import { color } from '@rneui/base';
+import { View } from 'react-native';
+
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-function FeedScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Feed Screen</Text>
-      <Button
-        title="Go to Profile"
-        onPress={() => navigation.navigate('Profile')}
-      />
-    </View>
-  );
-}
-
-function MessagesScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Messages Screen</Text>
-    </View>
-  );
-}
-
-function ProfileScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Profile Screen</Text>
-      <Button
-        title="Go to Settings"
-        onPress={() => navigation.navigate('Settings')}
-      />
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings Screen</Text>
-    </View>
-  );
-}
-
-function Home() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Feed" component={FeedScreen} />
-      <Tab.Screen name="Messages" component={MainPage} />
-    </Tab.Navigator>
-  );
-}
 
 function App() {
   return (
+  
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
@@ -70,6 +22,7 @@ function App() {
         />
       </Stack.Navigator>
     </NavigationContainer>
+
   );
 }
 
