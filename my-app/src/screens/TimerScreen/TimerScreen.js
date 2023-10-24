8ddf3310fddcf8cdb1943e91e4b1695f9a78cd01
mertCukadar <cukadar.mertkaan@gmail.com>
2023-10-24 22:17:52 +0300

    TimerScreen added

---
+++ my-app/src/screens/TimerScreen/TimerScreen.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { Timer } from "../../component/Timer";
+import { StrikeComp } from "../../component/Statistics/Strike";
+
+export function TimerScreen() {
+  const [running, setRunning] = useState(false);
+  return (
+    <View style={styles.container}>
+      <View style={styles.timerContainer}>
+        <Timer />
+      </View>
+
+      <View style = {styles.statusContainer}>
+      <Text style = {styles.statusText}>Status</Text>
+          <StrikeComp />
+      </View>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#24292E",
+  },
+  timerContainer: {
+    width: Dimensions.get("window").width * 0.9,
+    height: Dimensions.get("window").height * 0.5,
+    alignItems: "center",
+    borderRadius: 20,
+    justifyContent: "center",
+    backgroundColor: "#2B3137",
+  },
+
+  statusContainer: {
+    width: Dimensions.get("window").width * 0.9,
+    height: 200,
+    marginTop: 20,
+    alignItems: "center",
+    borderRadius: 20,
+    justifyContent: "center",
+    backgroundColor: "#2B3137",
+  },
+  buttonContainer: {
+    width: Dimensions.get("window").width * 0.9,
+    flexDirection: "column",
+    marginTop: 20,
+  },
+  statusText:
+    {
+        fontSize: 20,
+        color: '#19A7CE',
+        fontWeight: 'light',
+        marginTop: 20,
+    }
+});
+
