4e1572f6f1adc83aa2ba02bf69e9774b5bf42a00
mertCukadar <cukadar.mertkaan@gmail.com>
2023-10-24 15:12:33 +0300

    general update and package updates

--- my-app/src/component/NavigationBar/navigationBar.js
+++ my-app/src/component/NavigationBar/navigationBar.js
@@ -9,28 +9,28 @@ export function NavigationBar(){
             <View style = {styles.menuWrapper}>
             <Image
                 style={styles.image}
-                source={require('../../assets/Menu-İcons/Notification-icon.png')} 
+                source={require('../../assets/Menu-Icons/Notification-icon.png')} 
             />
             </View>
 
             <View style = {[styles.menuWrapper ,{borderColor : "#1d96b6" , borderWidth: 2,}]}>
             <Image
                 style={styles.image }
-                source={require('../../assets/Menu-İcons/Timer-icon.png')} 
+                source={require('../../assets/Menu-Icons/Timer-icon.png')} 
             />
             </View>
 
             <View style = {styles.menuWrapper}>
             <Image
                 style={styles.image}
-                source={require('../../assets/Menu-İcons/ToDo-icon.png')} 
+                source={require('../../assets/Menu-Icons/ToDo-icon.png')} 
             />
             </View>
 
             <View style = {styles.menuWrapper}>
             <Image
                 style={styles.image}
-                source={require('../../assets/Menu-İcons/Profile-icon.png')} 
+                source={require('../../assets/Menu-Icons/Notification-icon.png')} 
             />
             </View>
         </View>
