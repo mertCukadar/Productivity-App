8f6d6279d3051da887f52c12f05ebb0326f3a882
cukadarMert <cukadar.mertkaan@gmail.com>
2023-10-28 22:23:32 +0300

    todo screen refactoring

--- my-app/src/screens/TodoScreen/TodoScreen.js
+++ my-app/src/screens/TodoScreen/TodoScreen.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { StyleSheet, Text, Pressable, SafeAreaView , View, TextInput , ScrollView } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Dimensions } from 'react-native';
 import { TodoItem } from "../../component/Todo";
 
 
+function onPressFunction() {
+    console.log("pressed");
+}
 
 export function TodoScreen(props) {
     return (
-    
-           <View style={styles.container}>
+            <SafeAreaView style = {{flex : 1}}>
+                     <View style={styles.container}>
             
             {/* Search Component */}
                 <View style = {styles.searchWrapper}>
@@ -19,23 +23,39 @@ export function TodoScreen(props) {
             {/*Todo Component  */}
                 <View style = {styles.todoWrapper}>
                     <ScrollView>
-                    <TodoItem />
-                    <TodoItem />
-                    <TodoItem />
-                    <TodoItem />
+                    <TodoItem/>
                     <TodoItem />
                     <TodoItem />
                     <TodoItem />
 
+                    
+
                     </ScrollView>
+
+                    <View style = {styles.todoInputWrapper}>
+                        <Pressable onPress={onPressFunction} style = {styles.pressableWrapper}>
+                            <AntDesign name="plus" size={24} color="black" />
+                        </Pressable>
+                        <TextInput style = {styles.todoInput}></TextInput>
+                     </View>
+                  
                 </View>
 
             {/* DoneContainer */}
+              
                 <View style = {styles.doneWrapper}>
-                    <Text style = {{color : "white"}}>done panel</Text>
+                <ScrollView>
+                    <TodoItem isDone = {true}/>
+                    <TodoItem isDone = {true}/>
+                    <TodoItem isDone = {true}/>
+                    <TodoItem isDone = {true}/>
+
+                </ScrollView>
                 </View>
-            
-            </View>
+
+                </View>
+            </SafeAreaView>
+          
     
 
     );
@@ -57,10 +77,10 @@ const styles = StyleSheet.create({
         marginTop: 30,
         marginBottom: 30,
         width: Dimensions.get("window").width * 0.9,
-        height: Dimensions.get("window").height * 0.1,
+        height: Dimensions.get("window").height * 0.08,
     },
     todoWrapper: {
-        flex: 4,
+        flex: 3,
         alignItems: 'center',
         backgroundColor: '#2B3137',
         marginBottom: 30,
@@ -74,10 +94,45 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#2B3137',
         width: Dimensions.get("window").width * 0.9,
-        height: Dimensions.get("window").height * 0.5,
         borderRadius: 20,
+        opacity: 0.8,
         marginBottom: 30,
     },
+    todoInput: {
+        width: 300,
+        height: 50,
+        backgroundColor: '#30373E',
+        borderRadius: 15,
+        color: "white",
+        fontSize: 20,
+        paddingLeft: 20,
+   
+      },
+      
+    todoInputWrapper: {
+        justifyContent: "center",
+        alignItems: 'center',
+        height: 50,
+        marginBottom : 5,
+        flexDirection: "row",
+        marginTop: 5,
+        
+        
+    },
+    pressableWrapper: {
+        position: "absolute",
+        right: 0,
+        zIndex: 1,
+        width: 40,
+        height: 50,
+        borderRadius: 15,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#19A7CE",
+        
+        
+    },
+
 });
 
 export default TodoScreen;
