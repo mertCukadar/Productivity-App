158c5b9ac4e66e0642f60a0e2cd6ccb5d65afe50
mertCukadar <cukadar.mertkaan@gmail.com>
2023-10-31 18:49:10 +0300

    profile Infor screeen tavs added

--- my-app/src/screens/ProfileScreen/ProfileScreen.js
+++ my-app/src/screens/ProfileScreen/ProfileScreen.js
@@ -1,15 +1,28 @@
 import React from "react";
-import { StyleSheet, Text, Pressable, SafeAreaView , View, TextInput } from 'react-native';
+import { StyleSheet, Text, Pressable, SafeAreaView , View, TextInput , Button, Dimensions } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { TodoItem } from "../../component/Todo/TodoItem";
+import { useNavigation } from "@react-navigation/native";
+import { AntDesign } from '@expo/vector-icons';
+
+
 
 export function ProfileScreen(props) {
+    const navigation = useNavigation();
+
     return (
        
-           <View style={styles.container}>
-            <Text style = {{color:"white"}}>Profile Screen</Text>
+       <View style={styles.container}>
+                <Pressable onPress={() => navigation.navigate("ProfileInfoScreen")} style = {styles.pressableWrapper}>
+                    <AntDesign name="profile" size={24} color="white" style = {styles.antdesingWrapper} />
+                    <Text style = {{color:"white"}}>ProfileInfo</Text>
+                </Pressable>
 
-            </View>
+                <Pressable onPress={() => navigation.navigate("ProfileInfoScreen")} style = {styles.pressableWrapper}>
+                    <AntDesign name="barschart" size={24} color="white" style = {styles.antdesingWrapper} />
+                    <Text style = {{color:"white"}}>Statistics</Text>
+                </Pressable>
+        </View>
        
 
     );
@@ -22,5 +35,26 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#24292E',
     },
+    pressableWrapper: {
+        width: Dimensions.get("window").width * 0.9,
+        height: Dimensions.get("window").height * 0.1,
+        alignItems: 'center',
+        justifyContent: "center",
+        marginTop: 20,
+        backgroundColor: '#30373E',
+        borderRadius: 20,
+        flexDirection: "row",
+        padding: 20,
+        
+      
+    },
+    antdesingWrapper: {
+        marginRight: 20,
+
+    },
+
+
+    
+    
 });
 
