3feb8aed21877185d25a0ba333a33aa26c95cee1
mertCukadar <cukadar.mertkaan@gmail.com>
2023-11-22 14:19:59 +0300

    jwt from secure store added

--- my-app/App.js
+++ my-app/App.js
@@ -6,6 +6,10 @@ import { CustomTabNavigator } from './src/component/CustomTabNavigator/customTab
 import { ProfileInfoScreen } from './src/screens/ProfileInfoScreen';
 import { LoginScreen } from './src/screens/LoginScreen';
 import * as SystemUI from "expo-system-ui";
+import { Spinner } from './src/component/Spinner';
+import * as SecureStore from 'expo-secure-store';
+import { useCallback } from 'react';
+import { useEffect , useState} from 'react';
 import { AuthContext, AuthProvider } from './src/context/AuthContext';
 import { axiosContext, AxiosProvider } from './src/context/axiosContext'; // AxiosProvider import ekledim
 
@@ -25,12 +29,57 @@ const AppWrapper = () => {
 
 function App() {
   const { authState, setAuthState, getAccessToken } = useContext(AuthContext);
-  const LoadJWT = useContext(axiosContext);
+  const authAxios = useContext(axiosContext);
+
+  const [status, setStatus] = useState('loading');
+
+  const loadJWT = useCallback(async () => {
+    try {
+      const jwt = await SecureStore.getItemAsync('accessToken');
+      console.log('JWT from SecureStore:', jwt);
+  
+      if (jwt) {
+        setAuthState({
+          accessToken: jwt,
+          refreshToken: null, // You may need to handle refresh token separately if needed
+          authenticated: true,
+        });
+        setStatus('success');
+      } else {
+        setStatus('error');
+        console.log('Stored JWT is empty.');
+        setAuthState({
+          accessToken: null,
+          refreshToken: null,
+          authenticated: false,
+        });
+      }
+    } catch (error) {
+      setStatus('error');
+      console.log(`SecureStore Error: ${error.message}`);
+      setAuthState({
+        accessToken: null,
+        refreshToken: null,
+        authenticated: false,
+      });
+    }
+  }, []);
+  
+  
+
+  useEffect(() => {
+    loadJWT();
+  }, [loadJWT]);
+
+  if (status === 'loading') {
+    return <Spinner />;
+  }
+  
 
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {authState.authenticated === null || authState.authenticated === false ? (
+        {authState.authenticated === false ? (
           <Stack.Screen
             name="LoginScreen"
             component={LoginScreen}
@@ -53,6 +102,12 @@ function App() {
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+  
+  
+
+};
+
+ 
+
 
 export default AppWrapper;
